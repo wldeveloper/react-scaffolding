e283aab2e1c6df9a4a67a4d8004e488aa489e950
wl <wl@51box.cn>
2017-06-23 11:32:51 +0800

    add stage-0

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['env','react']
+                        presets: ['env','react', 'stage-0']
                     }
                 }
             },
